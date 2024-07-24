e40eae088ac6d398a4b16a90743bbb5a1db60f4e
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-24 21:45:14 +0545

    feat: register and login route created

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import AuthController from '../controllers/auth.controller';
+import { CreateUserDTO, LoginUserDTO } from '../dto/auth.dto';
+import RequestValidator from '../middleware/Request.Validator';
+import { catchAsync } from '../utils/catchAsync.utils';
+
+const router = express.Router();
+
+const authController = new AuthController();
+// Endpoint for creating user
+router.post(
+    '/register',
+    RequestValidator.validate(CreateUserDTO),
+    catchAsync(authController.createUser)
+);
+
+router.post(
+    '/login',
+    RequestValidator.validate(LoginUserDTO),
+    catchAsync(authController.login)
+);
+
+export default router;
