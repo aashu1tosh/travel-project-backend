46a35ef39ff00bcbfb0c520ee78ca7693a4300e1
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-24 21:48:03 +0545

    feat: bcrypt service class created

---
+++ src/services/bcrypt.service.ts
@@ -0,0 +1,11 @@
+import bcrypt from 'bcryptjs';
+
+export class BcryptService {
+    async hash(password: string): Promise<string> {
+        return await bcrypt.hash(password, 10);
+    }
+
+    async compare(password: string, hash: string): Promise<boolean> {
+        return await bcrypt.compare(password, hash);
+    }
+}
