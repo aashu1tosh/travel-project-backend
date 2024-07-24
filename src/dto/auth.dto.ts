bbd81f2c75f0a16a8f8cfb04f2167d4ccc98fd36
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-24 21:43:27 +0545

    feat: dto for register, login, update password, google oauth login

---
+++ src/dto/auth.dto.ts
@@ -0,0 +1,92 @@
+import { Type as CType } from 'class-transformer';
+import {
+    IsDefined,
+    IsEmail,
+    IsEnum,
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    Matches,
+    ValidateNested,
+} from 'class-validator';
+import { ROLE } from '../constant/enum';
+import { passwordRegex, phoneNumberRegex } from '../constant/regex';
+import { Message } from './../constant/messages';
+
+export class DetailsDTO {
+    @IsNotEmpty()
+    @IsString()
+    firstName: string;
+
+    @IsOptional()
+    @IsNotEmpty()
+    @IsString()
+    middleName: string;
+
+    @IsNotEmpty()
+    @IsString()
+    lastName: string;
+
+    @IsNotEmpty()
+    @Matches(phoneNumberRegex, {
+        message: Message.validPhoneNumber,
+    })
+    phoneNumber: string;
+}
+
+export class CreateUserDTO {
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @ValidateNested()
+    @IsDefined()
+    @CType(() => DetailsDTO)
+    details: DetailsDTO;
+
+    @IsNotEmpty()
+    @Matches(passwordRegex, {
+        message: Message.passwordShouldBeStrong,
+    })
+    password: string;
+
+    @IsNotEmpty()
+    @IsEnum(ROLE)
+    role: string;
+}
+
+export class RequestEmailVerification {
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+}
+
+export class LoginUserDTO {
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @Matches(passwordRegex, {
+        message: Message.passwordShouldBeStrong,
+    })
+    password: string;
+}
+
+export class GoogleLoginDTO {
+    @IsNotEmpty()
+    @IsString()
+    googleId: string;
+}
+
+export class UpdatePasswordDTO {
+    @IsNotEmpty()
+    oldPassword: string;
+
+    @IsNotEmpty()
+    @Matches(passwordRegex, {
+        message: Message.passwordShouldBeStrong,
+    })
+    newPassword: string;
+}
