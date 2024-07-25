55855d3a3a277899ebe45ae02ab1f7128965cda4
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-25 12:00:06 +0545

    feat: news letter route defined

---
+++ src/routes/user.route.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { catchAsync } from '../utils/catchAsync.utils';
+import UserController from './../controllers/user.controller';
+import { EmailOnlyDto } from './../dto/user.dto';
+import RequestValidator from './../middleware/Request.Validator';
+// import { EmailOnlyDto } from 'dto/user.dto';
+
+const router = express.Router();
+
+const userController = new UserController();
+// Endpoint for creating user
+router.post(
+    '/news-letter',
+    RequestValidator.validate(EmailOnlyDto),
+    catchAsync(userController.newsLetter)
+);
+
+
+
+export default router;
