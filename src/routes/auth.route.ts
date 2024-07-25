642f42405ddf77a4b1551fedace86c5fd0462999
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-25 09:55:20 +0545

    feat: request verification and verify route created

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import AuthController from '../controllers/auth.controller';
-import { CreateUserDTO, LoginUserDTO } from '../dto/auth.dto';
+import {
+    CreateUserDTO,
+    LoginUserDTO,
+    RequestEmailVerificationDTO,
+} from '../dto/auth.dto';
 import RequestValidator from '../middleware/Request.Validator';
 import { catchAsync } from '../utils/catchAsync.utils';
 
@@ -13,6 +17,16 @@ router.post(
     RequestValidator.validate(CreateUserDTO),
     catchAsync(authController.createUser)
 );
+
+//
+router.post(
+    '/request-verification',
+    RequestValidator.validate(RequestEmailVerificationDTO),
+    catchAsync(authController.requestVerification)
+);
+
+//Endpoint for verification email
+router.get('/verify/:token', catchAsync(authController.verifyEmail));
 
 router.post(
     '/login',
