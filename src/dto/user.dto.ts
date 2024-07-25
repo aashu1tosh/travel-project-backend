8ac05af92426dcdf9a2e0c4ae90e511dd3eb35e6
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-25 14:26:05 +0545

    feat: contact form dto created

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,7 +1,33 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
+import { Message } from './../constant/messages';
+import { phoneNumberRegex } from './../constant/regex';
 
-export class EmailOnlyDto {
+export class EmailOnlyDTO {
     @IsNotEmpty()
     @IsEmail()
     email: string;
+}
+
+export class ContactFormDTO {
+    @IsNotEmpty()
+    @IsString()
+    fullName: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @Matches(phoneNumberRegex, {
+        message: Message.validPhoneNumber,
+    })
+    phoneNumber: string;
+
+    @IsNotEmpty()
+    @IsString()
+    subject: string;
+
+    @IsNotEmpty()
+    @IsString()
+    message: string;
 }
