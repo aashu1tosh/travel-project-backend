2f525b9950f295151b3a0e32a8ebfb046d0d5c6f
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-25 09:51:42 +0545

    feat: request verification and verify email controllers created

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -12,6 +12,24 @@ class AuthController {
         });
     }
 
+    async requestVerification(req: Request, res: Response) {
+        const email = req?.body?.email;
+        await authServices.requestVerification(email);
+        res.status(StatusCodes.SUCCESS).json({
+            success: true,
+            message: Message.emailSent,
+        });
+    }
+
+    async verifyEmail(req: Request, res: Response) {
+        const token = req.params.token;
+        const response = await authServices.verifyEmail(token as string);
+        res.status(StatusCodes.ACCEPTED).json({
+            success: true,
+            message: response,
+        });
+    }
+
     async login(req: Request, res: Response) {
         const response = await authServices.login(req.body);
         res.status(StatusCodes.CREATED).json({
