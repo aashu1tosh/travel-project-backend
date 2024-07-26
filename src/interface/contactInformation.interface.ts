39d024c3b2c5e804ef021e7e3246764f74fc417a
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-26 10:26:48 +0545

    feat: interface for contact inforamtion created

---
+++ src/interface/contactInformation.interface.ts
@@ -0,0 +1,13 @@
+export interface IContactInformation {
+    location: string;
+    lat: string;
+    long: string;
+    email: string;
+    availableDays: string;
+    availableTime: string;
+    phoneNumber: string;
+    secondaryPhoneNumber: string;
+    facebookLink: string;
+    instagramLink: string;
+    twitterLink: string;
+}
