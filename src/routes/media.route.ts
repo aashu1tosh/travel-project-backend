1456708b04705b17e0085115a31f632b5db1513d
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-26 17:29:23 +0545

    feat: added authentication

--- src/routes/media.route.ts
+++ src/routes/media.route.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import mediaController from './../controllers/media.controller';
+import { authentication } from './../middleware/authentication.middleware';
 import { catchAsync } from './../utils/catchAsync.utils';
 import uploadFile from './../utils/fileUpload';
 const router = express.Router();
 
+router.use(authentication());
+
+// route for uploading multiple files
 router.post('/', uploadFile.array('file'), catchAsync(mediaController.create));
+
+// route for uploading single file
 router.post(
     '/single',
     uploadFile.single('file'),
