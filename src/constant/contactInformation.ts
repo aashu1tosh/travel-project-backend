f355fd9ccfc0f86b381add28485b0f41b18ac9cd
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-26 10:22:46 +0545

    data: contact information data mapped

---
+++ src/constant/contactInformation.ts
@@ -0,0 +1,13 @@
+export const contactInformation = {
+    location: 'Mahadevsthan Marga, Koteshwor, Kathmandu',
+    lat: "27.6966894",
+    long: "85.3330069",
+    email: 'contact@infocare.com',
+    availableDays: 'Monday - Saturday',
+    availableTime: '9AM - 5PM',
+    phoneNumber: '9851022662',
+    secondaryPhoneNumber: '9851022662',
+    facebookLink: 'https://www.facebook.com/',
+    instagramLink: 'https://www.instagram.com/',
+    twitterLink: 'https://www.x.com'
+}
