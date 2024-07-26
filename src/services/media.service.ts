145815552360c8687575cfd7161c0995bfc17d2a
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-26 17:30:33 +0545

    feat: linked auth id with media entity

--- src/services/media.service.ts
+++ src/services/media.service.ts
@@ -1,6 +1,7 @@
 // import { MediaType } from 'express';
 import { AppDataSource } from './../config/database.config';
 import { MediaType } from './../constant/enum';
+import { Auth } from './../entities/auth/auth.entity';
 import Media from './../entities/media.entity';
 
 class MediaService {
@@ -8,11 +9,12 @@ class MediaService {
         private readonly mediaRepo = AppDataSource.getRepository(Media)
     ) {}
 
-    async create(name: string, mimeType: string, type: MediaType) {
+    async create(name: string, mimeType: string, type: MediaType, id: string) {
         const data = this.mediaRepo.create();
         data.name = name;
         data.mimeType = mimeType;
         data.type = type;
+        data.auth = { id } as Auth;
         const response = await this.mediaRepo.save(data);
         response.transferImageFromTempToUploadFolder(
             response.id as string,
