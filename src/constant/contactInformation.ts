6085ad4908fea8de4cdc930141c4b92ce63777a7
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-26 10:24:00 +0545

    style: format check

--- src/constant/contactInformation.ts
+++ src/constant/contactInformation.ts
@@ -1,7 +1,7 @@
 export const contactInformation = {
     location: 'Mahadevsthan Marga, Koteshwor, Kathmandu',
-    lat: "27.6966894",
-    long: "85.3330069",
+    lat: '27.6966894',
+    long: '85.3330069',
     email: 'contact@infocare.com',
     availableDays: 'Monday - Saturday',
     availableTime: '9AM - 5PM',
@@ -9,5 +9,5 @@ export const contactInformation = {
     secondaryPhoneNumber: '9851022662',
     facebookLink: 'https://www.facebook.com/',
     instagramLink: 'https://www.instagram.com/',
-    twitterLink: 'https://www.x.com'
-}
+    twitterLink: 'https://www.x.com',
+};
--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -2,14 +2,23 @@ import { type Request, type Response } from 'express';
 import { Message, deletedMessage } from './../constant/messages';
 import { StatusCodes } from './../constant/statusCodes';
 import adminService from './../services/admin.service';
+import { getPaginationData, paginationValidator } from './../utils/pagination';
 
 class AdminController {
     async getAllUser(req: Request, res: Response) {
-        const response = await adminService.getAllUser();
+        const [page, perpage] = paginationValidator(
+            req.query.page as string,
+            req.query.perpage as string
+        );
+        const response = await adminService.getAllUser(page, perpage);
         res.status(StatusCodes.SUCCESS).json({
             success: true,
             message: Message.fetched,
-            data: response,
+            data: {
+                data: response.data,
+                pagination: getPaginationData(response.total, page, perpage)
+            }
+
         });
     }
 
