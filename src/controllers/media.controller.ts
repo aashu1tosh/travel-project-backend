44b4f56f0f1e44c19eae9d4b84ba9f1be0a82786
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-29 12:27:56 +0545

    fix: error response fixed

--- src/controllers/media.controller.ts
+++ src/controllers/media.controller.ts
@@ -8,26 +8,26 @@ import mediaService from './../services/media.service';
 class MediaController {
     async create(req: Request, res: Response) {
         if (req?.files?.length === 0)
-            throw HttpException.badRequest('Sorry file could not be uploaded.');
+            throw HttpException.badRequest('Sorry media could not be uploaded.');
 
         const files = req?.files;
         const authId = req?.user?.id as string;
         const idList = Array.isArray(files)
             ? await Promise.all(
-                  files?.map(async (file: any) => {
-                      const name = file?.filename;
-                      const mimeType = file?.mimetype;
-                      const type = req.body?.type as MediaType;
-                      const response = await mediaService.create(
-                          name,
-                          mimeType,
-                          type,
-                          authId
-                      );
-                      return response.id;
-                  })
-              )
-            : HttpException.internalServerError('Media upload failed');
+                files?.map(async (file: any) => {
+                    const name = file?.filename;
+                    const mimeType = file?.mimetype;
+                    const type = req.body?.type as MediaType;
+                    const response = await mediaService.create(
+                        name,
+                        mimeType,
+                        type,
+                        authId
+                    );
+                    return response.id;
+                })
+            )
+            : HttpException.internalServerError('Sorry media upload failed');
 
         const data = { mediaId: idList };
         res.status(StatusCodes.CREATED).json({
@@ -44,7 +44,7 @@ class MediaController {
             Array.isArray(req?.files) &&
             (req.files.length === 0 || req.files.length >= 2)
         )
-            throw HttpException.badRequest('Sorry file could not be uploaded.');
+            throw HttpException.badRequest('Sorry media could not be uploaded.');
 
         const file = req?.file;
         const authId = req?.user?.id as string;
