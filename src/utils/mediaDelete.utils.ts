96ea3ed7f673aea8d20be34e7960db11bd4343be
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-29 14:25:25 +0545

    feat: function to delete media file

--- src/utils/mediaDelete.utils.ts
+++ src/utils/mediaDelete.utils.ts
@@ -1,10 +1,13 @@
 import fs from 'fs';
 import path from 'path';
+import { DotenvConfig } from './../config/env.config';
 import Print from './print';
 
 export const deleteMedia = (filepath: string): Promise<boolean> => {
-    const imagePath = path.join(__dirname, '../', '../').concat(filepath);
-
+    const mediaPath = filepath.split(DotenvConfig.PORT.toString())[1];
+    const imagePath = path
+        .join(__dirname, '../', '../', './public/uploads')
+        .concat(mediaPath);
     return new Promise((resolve, reject) => {
         fs.unlink(imagePath, (error) => {
             if (error) {
